5c439c0b58773dbb1541475f85d828449b115ac9
Yossi Tubis <Yossi.Tubis@harman.com>
2016-07-13 15:54:44 +0300

    BT Pairing

--- SniperIR/src/com/example/sniperir/MainActivity.java
+++ SniperIR/src/com/example/sniperir/MainActivity.java
@@ -1,15 +1,181 @@
 package com.example.sniperir;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.example.sniperir.Device;
+import com.example.sniperir.MainActivity;
+import com.example.sniperir.R;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.DialogInterface.OnKeyListener;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private static final String LOG = "Mainactivity";
+	private BluetoothAdapter mBluetoothAdapter;
+	private static final int REQUEST_ENABLE_BT = 1;
+	private static final long SCAN_PERIOD = 3000;
+	private Dialog mDialog;
+	public static List<BluetoothDevice> mDevices = new ArrayList<BluetoothDevice>();
+	public static MainActivity instance = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        if (!getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_BLUETOOTH_LE)) {
+			Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
+					.show();
+			finish();
+		}
+
+		final BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		mBluetoothAdapter = mBluetoothManager.getAdapter();
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT)
+					.show();
+			finish();
+			return;
+		}
+
+		if (!mBluetoothAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		
+		Button btn = (Button)findViewById(R.id.btn);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				scanLeDevice();
+
+				showRoundProcessDialog(MainActivity.this, R.layout.loading_process_dialog_anim);
+
+				Timer mTimer = new Timer();
+				mTimer.schedule(new TimerTask() {
+
+					@Override
+					public void run() {
+						Intent deviceListIntent = new Intent(getApplicationContext(),
+								Device.class);
+						startActivity(deviceListIntent);
+						Log.i(LOG, "run(1) ");
+						mDialog.dismiss();
+					}
+				}, SCAN_PERIOD);
+			}
+		});
+
+		scanLeDevice();
+
+		showRoundProcessDialog(MainActivity.this, R.layout.loading_process_dialog_anim);
+
+		Timer mTimer = new Timer();
+		mTimer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				Intent deviceListIntent = new Intent(getApplicationContext(),
+						Device.class);
+				startActivity(deviceListIntent);
+				Log.i(LOG, "run(2) ");
+				if (mDialog != null)
+				mDialog.dismiss();
+			}
+		}, SCAN_PERIOD);
+
+		instance = this;
     }
+    
+	public void showRoundProcessDialog(Context mContext, int layout) {
+		OnKeyListener keyListener = new OnKeyListener() {
+			@Override
+			public boolean onKey(DialogInterface dialog, int keyCode,
+					KeyEvent event) {
+				if (keyCode == KeyEvent.KEYCODE_HOME
+						|| keyCode == KeyEvent.KEYCODE_SEARCH) {
+					return true;
+				}
+				return false;
+			}
+		};
+
+		mDialog = new AlertDialog.Builder(mContext).create();
+		mDialog.setOnKeyListener(keyListener);
+		mDialog.show();
+		// 娉ㄦ��姝ゅ��瑕���惧��show涔���� ������浼���ュ��甯�
+		mDialog.setContentView(layout);
+	}
+
+	private void scanLeDevice() {
+		new Thread() {
+
+			@Override
+			public void run() {
+				mBluetoothAdapter.startLeScan(mLeScanCallback);
+
+				try {
+					Thread.sleep(SCAN_PERIOD);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				mBluetoothAdapter.stopLeScan(mLeScanCallback);
+			}
+		}.start();
+	}
+
+	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
+		@Override
+		public void onLeScan(final BluetoothDevice device, final int rssi,
+				byte[] scanRecord) {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if (device != null) {
+						if (mDevices.indexOf(device) == -1)
+							mDevices.add(device);
+					}
+				}
+			});
+		}
+	};
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// User chose not to enable Bluetooth.
+		if (requestCode == REQUEST_ENABLE_BT
+				&& resultCode == Activity.RESULT_CANCELED) {
+			finish();
+			return;
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
 }
